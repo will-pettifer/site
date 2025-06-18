7c2bb134f11be915e41d0fe3e3e1313319a687d2
will-pettifer <billy.pettifer@gmail.com>
2025-06-18 12:41:58 +0100

    fixed sites

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://will-pettifer.github.io',
-    base: '/my_website_2',
+    base: '/site',
     markdown: {
         shikiConfig: {
             theme: 'github-dark'
