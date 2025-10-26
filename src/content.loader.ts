80ac6750350ef0d1e68f11f7b64b0a892caec698
William J G Pettifer <billy.pettifer@gmail.com>
2025-10-26 10:24:47 +0000

    Testing Draft exclusions

--- src/content.loader.ts
+++ src/content.loader.ts
@@ -8,7 +8,7 @@ export interface Post {
 
 export async function getPosts(): Promise<Post[]> {
 
-    const posts = import.meta.glob('./content/**/*.md', { eager: true });
+    const posts = import.meta.glob('./content/**/*.md', {eager: true});
 
     return Object.entries(posts).map(([filepath, post]: [string, any]) => {
         const slug = filepath
@@ -19,13 +19,17 @@ export async function getPosts(): Promise<Post[]> {
             slug,
             title: post.frontmatter?.title || slug.split('/').pop(),
             date,
+            draft: post.frontmatter?.draft || false,
             body: post.compiledContent(),
             url: '/site/' + slug
         };
-    }).sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    }).filter(post => {
+        return import.meta.env.PROD ? !post.draft : true;
+    }).map(({draft, ...post}) => post)
+        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 }
 
 export async function getPostsMetadata(): Promise<Omit<Post, 'body'>[]> {
     const posts = await getPosts();
-    return posts.map(({ body, ...metadata }) => metadata);
+    return posts.map(({body, ...metadata}) => metadata);
 }
